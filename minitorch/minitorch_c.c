5a58f5bd6babc1433414761b9fef84fc86b9238d
BuildTools <davidmaamoaix@gmail.com>
2020-08-30 13:34:49 +0800

    More tensor operations

--- minitorch/minitorch_c.c
+++ minitorch/minitorch_c.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 /* tensor definition */
+
 typedef struct {
 	PyObject_HEAD
 
@@ -9,25 +10,71 @@ typedef struct {
 	int length;
 } Tensor;
 
-static PyObject *newTensor(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
+static PyObject *newTensor(PyTypeObject *type, PyObject *args, PyObject *kw) {
 	Tensor *self = (Tensor *) type->tp_alloc(type, 0);
 
-	if (self != NULL) {
+	return (PyObject *) self;
+}
 
-		if (!PyArg_ParseTuple(args, "i", &self->length)) {
-			return NULL;
-		}
-
-		self->values = malloc(self->length * sizeof(double));
+static int initTensor(Tensor *self, PyObject *args, PyObject *kw) {
+	if (!PyArg_ParseTuple(args, "i", &self->length)) {
+		return -1;
 	}
 
-	return (PyObject *) self;
+	self->values = malloc(self->length * sizeof(double));
+
+	return 0;
 }
 
 static void deallocTensor(Tensor *self) {
 	free(self->values);
 	Py_TYPE(self)->tp_free((PyObject *) self);
 }
+
+
+/* tensor properties */
+
+static Py_ssize_t tensorLength(Tensor *self) {
+	return self->length;
+}
+
+static PyObject *indexTensor(Tensor *self, Py_ssize_t i) {
+	if (i >= self->length) {
+		PyErr_Format(
+			PyExc_IndexError,
+			"Index %d is out of bounds of tensor with length %d",
+			i, self->length
+		);
+		return NULL;
+	}
+
+	return PyFloat_FromDouble(self->values[i]);
+}
+
+static int assignTensor(Tensor *self, Py_ssize_t i, PyObject *value) {
+	if (i >= self->length) {
+		PyErr_Format(
+			PyExc_IndexError,
+			"Index %d is out of bounds of tensor with length %d",
+			i, self->length
+		);
+		return -1;
+	}
+
+	self->values[i] = PyFloat_AsDouble(value);
+
+	return 0;
+}
+
+static PyMethodDef tensorMethodTable[] = {
+	{NULL, NULL, 0, NULL}
+};
+
+static PySequenceMethods tensorSequence = {
+	.sq_length = (lenfunc) tensorLength,
+	.sq_item = (ssizeargfunc) indexTensor,
+	.sq_ass_item = (ssizeobjargproc) assignTensor
+};
 
 static PyTypeObject TensorType = {
 	PyVarObject_HEAD_INIT(NULL, 0)
@@ -37,8 +84,11 @@ static PyTypeObject TensorType = {
 	.tp_basicsize = sizeof(Tensor),
 	.tp_itemsize = 0,
 	.tp_flags = Py_TPFLAGS_DEFAULT,
-	.tp_new = PyType_GenericNew,
-	.tp_dealloc = (destructor) deallocTensor
+	.tp_new = newTensor,
+	.tp_init = (initproc) initTensor,
+	.tp_dealloc = (destructor) deallocTensor,
+	.tp_methods = tensorMethodTable,
+	.tp_as_sequence = &tensorSequence
 };
 
 
@@ -72,6 +122,17 @@ static struct PyModuleDef minitorch = {
 
 PyMODINIT_FUNC PyInit_minitorch_c() {
 	PyObject *module = PyModule_Create(&minitorch);
+
+	if (PyType_Ready(&TensorType) < 0) {
+		return NULL;
+	}
+
+	Py_INCREF(&TensorType);
+	if (PyModule_AddObject(module, "Tensor", (PyObject *) &TensorType) < 0) {
+		Py_DECREF(&TensorType);
+		Py_DECREF(module);
+		return NULL;
+	}
 
 	return module;
 }
